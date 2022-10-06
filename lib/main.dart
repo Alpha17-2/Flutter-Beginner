1744bb200217976bea5ce0ba43fba782e0359d6b
Subhojeet Sahoo <alphaisgod1@gmail.com>
2022-10-06 16:56:33 +0530

    Radhe Radhe

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:test_app/screens/home_screen.dart';
+
+// This is the entry point of dart code
+void main() {
+  runApp(TestApp());
+}
+
+class TestApp extends StatelessWidget {
+  const TestApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
