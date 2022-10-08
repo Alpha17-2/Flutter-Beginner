f5fc281c76cb29705c4a9d880f6a55f4df823a5f
Abhi-kr21 <abhishek7858010345@gmail.com>
2022-10-08 10:53:53 +0530

    Homescreen updated

--- lib/screens/HomeScreen/home_screen.dart
+++ lib/screens/HomeScreen/home_screen.dart
@@ -6,8 +6,19 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment.center,
-      child: Text("HomeScreen"),
-    );
+        alignment: Alignment.center,
+        child: Column(
+          children: [
+            Image.network(
+                "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png"),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Welcome to flutter",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ));
   }
 }
